9333a476842e55c8be73c42e7d64fcd48d6db4db
larsandreasvestbo <lars.andreas.vestbo@appex.no>
2022-11-16 11:36:06 +0100

    Initial

---
+++ MemoryReader/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Console.Write("Process Id: ");
+var idString = Console.ReadLine();
+
+if (int.TryParse(idString, out var id))
+{
+    var process = Process.GetProcessById(id);
+    process.MainModule.
+}
---
+++ SimpleApplication/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var test = "This is a string";
+
+while (true)
+{
+    Console.WriteLine(test);
+    Console.ReadLine();
+}
